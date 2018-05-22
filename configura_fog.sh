90ded38db99d0abf9ecd3526beae2053bf16e9e6
root <root@debian>
2018-05-22 18:56:28 +0200

    Actualizacion

--- configura_fog.sh
+++ configura_fog.sh
@@ -2,4 +2,5 @@
 
 wget https://github.com/FOGProject/fogproject/archive/1.5.2.tar.gz
 tar -xvzf 1.5.2.tar.gz
-./fogproject-1.5.2/bin/installfog.sh
+cd fogproject-1.5.2/bin/
+(echo -e "Y\n" & echo -e "N\n" & echo -e "N\n" & echo -e "n\n" & echo -e "n\n" & echo -e "N\n" & echo -e "192.168.1.19\n" & echo -e "N\n" & echo -e "2\n") | ./installfog.sh
