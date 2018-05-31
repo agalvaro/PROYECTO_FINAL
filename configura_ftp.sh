28f2edac0944bb4196f5b31c2981ccb3f7b8eac1
root <root@debian>
2018-05-31 14:18:53 +0200

    Actualizacion

--- configura_ftp.sh
+++ configura_ftp.sh
@@ -3,6 +3,6 @@
 apt-get install -y proftpd-basic proftpd-doc
 
 rm /etc/proftpd/proftpd.conf
-cp /POYECTO_FINAL/archivo_proftpd/proftpd.conf /etc/proftpd/proftpd.conf
+cp PROYECTO_FINAL/archivo_proftpd/proftpd.conf /etc/proftpd/proftpd.conf
 
 /etc/init.d/proftpd restart
